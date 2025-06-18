2dfd1af77677da285678752406ae86b25ec77963
Krzysztof Wojciechowski <kwojciecho@laas.fr>
2025-06-18 12:52:44 +0000

    Add two-level threshold

--- include/panda_deburring/contact_detector.hpp
+++ include/panda_deburring/contact_detector.hpp
@@ -9,14 +9,10 @@ class ContactDetector {
   ContactDetector() {}
 
   void update(const pinocchio::Force &force) {
-    last_measured_state_ = force.linear().norm() > thresh_;
+    const double thresh =
+        filterted_state_ ? lower_threshold_ : upper_threshold_;
+    last_measured_state_ = force.linear().norm() > thresh;
   }
-
-  void set_hysteresis_samples(const unsigned int hysteresis_samples) {
-    hysteresis_samples_ = hysteresis_samples;
-  }
-
-  unsigned int get_hysteresis_samples() const { return hysteresis_samples_; }
 
   bool in_contact() {
     if (last_measured_state_ != filterted_state_ &&
@@ -30,12 +26,31 @@ class ContactDetector {
     return filterted_state_;
   }
 
+  void set_hysteresis_samples(const unsigned int hysteresis_samples) {
+    hysteresis_samples_ = hysteresis_samples;
+  }
+
+  unsigned int get_hysteresis_samples() const { return hysteresis_samples_; }
+
+  void set_lower_threshold(const double lower_threshold) {
+    lower_threshold_ = lower_threshold;
+  }
+
+  double get_lower_threshold() const { return lower_threshold_; }
+
+  void set_upper_threshold(const double upper_threshold) {
+    upper_threshold_ = upper_threshold;
+  }
+
+  double get_upper_threshold() const { return upper_threshold_; }
+
  private:
   bool last_measured_state_ = false;
   bool filterted_state_ = false;
-  const double thresh_ = 0.0;
   unsigned int samples_since_switch_ = 0;
   unsigned int hysteresis_samples_ = 0;
+  double lower_threshold_ = 0.0;
+  double upper_threshold_ = 0.0;
 };
 }  // namespace ft_calibration_filter
 
